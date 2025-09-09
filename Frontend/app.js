8a65f5277b896f12070499b73a9e01c52c55ced9
LUIS BELLOCH <bellochluis@gmail.com>
2025-09-09 12:04:44 +0200

    Frontend post, put, delete calls

--- Frontend/app.js
+++ Frontend/app.js
@@ -2,16 +2,74 @@ const login_btn = document.querySelector("#login-btn");
 const container = document.querySelector(".container");
 
 login_btn.addEventListener('click', () =>{
-    httpGetAsync("http://localhost/agenda/api.php")
+    httpGetAsync("http://localhost/agenda/api.php", loadTable)
 });
 
-function httpGetAsync(theUrl, callback)
+function loadTable() {
+
+}
+
+function getLogin(url, user, password, callback)
 {
-    var xmlHttp = new XMLHttpRequest();
-    xmlHttp.onreadystatechange = function() { 
-        if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
-            callback(xmlHttp.responseText);
+    fetch(url, {
+    method: "GET",
+    body: JSON.stringify({
+        table: 'user',
+        username: user,
+        password: password,
+        completed: false
+    }),
+    headers: {
+        "Content-type": "application/json; charset=UTF-8"
     }
-    xmlHttp.open("GET", theUrl, true); // true for asynchronous 
-    xmlHttp.send(null);
+    }).then((result) => {
+        callback()
+    });
+}
+
+function createContact(url, _name, _email, _phone) {
+    fetch(url, {
+    method: "POST",
+    body: JSON.stringify({
+        table: 'contact',
+        name: _name,
+        email: _email,
+        phone: _phone,
+        completed: false
+    }),
+    headers: {
+        "Content-type": "application/json; charset=UTF-8"
+    }
+    });
+}
+
+function updateContact(url, _name, _email, _phone, _id) {
+    fetch(url, {
+    method: "PUT",
+    body: JSON.stringify({
+        table: 'contact',
+        name: _name,
+        email: _email,
+        phone: _phone,
+        id: _id,
+        completed: false
+    }),
+    headers: {
+        "Content-type": "application/json; charset=UTF-8"
+    }
+    });
+}
+
+function deleteContact(url, _id) {
+    fetch(url, {
+    method: "DELETE",
+    body: JSON.stringify({
+        table: 'contact',
+        id: _id,
+        completed: false
+    }),
+    headers: {
+        "Content-type": "application/json; charset=UTF-8"
+    }
+    });
 }
