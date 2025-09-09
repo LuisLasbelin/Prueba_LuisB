efff05d4f9e99dfd2ed91abd9688f80c8a8fd52c
LUIS BELLOCH <bellochluis@gmail.com>
2025-09-09 11:36:36 +0200

    Api get post user y contacto

---
+++ Backend/api.php
@@ -0,0 +1,71 @@
+<?php
+include 'db.php';
+
+header("Content-Type: application/json");
+
+$method = $_SERVER['REQUEST_METHOD'];
+$input = json_decode(file_get_contents('php://input'), true);
+
+switch ($method) {
+    case 'GET':
+        if (isset($_GET['id'])) {
+            $table = $input['table'];
+            if ($table == "user") {
+                $id = $_GET['id'];
+                $result = $conn->query("SELECT * FROM user WHERE id=$id");
+                $data = $result->fetch_assoc();
+                echo json_encode($data);
+            }
+            if ($table == "contact-list") {
+                $result = $conn->query("SELECT * FROM contact WHERE *");
+                $data = $result->fetch_assoc();
+                echo json_encode($data);
+            }
+        }
+        break;
+
+    // USERS
+
+    case 'POST':
+        $table = $input['table'];
+        if ($table == "user") {
+            $name = $input['name'];
+            $email = $input['email'];
+            $conn->query("INSERT INTO user (name, email) VALUES ('$name', '$email')");
+            echo json_encode(["message" => "User added successfully"]);
+            break;
+        }
+        if ($table == "contact") {
+            $name = $input['name'];
+            $email = $input['email'];
+            $email = $input['phone'];
+            $conn->query("INSERT INTO contact (name, email, phone) VALUES ('$name', '$email', '$phone)");
+            echo json_encode(["message" => "Contact added successfully"]);
+            break;
+        }
+
+
+    case 'PUT':
+        $id = $_GET['id'];
+        $table = $input['table'];
+        $name = $input['name'];
+        $email = $input['email'];
+        $conn->query("UPDATE $table SET name='$name',
+                     email='$email', age=$age WHERE id=$id");
+        echo json_encode(["message" => "User updated successfully"]);
+        break;
+
+    case 'DELETE':
+        $id = $_GET['id'];
+        $table = $input['table'];
+        $conn->query("DELETE FROM $table WHERE id=$id");
+        echo json_encode(["message" => "User deleted successfully"]);
+        break;
+
+    default:
+        echo json_encode(["message" => "Invalid request method"]);
+        break;
+}
+
+$conn->close();
+?>
--- Frontend/app.js
+++ Frontend/app.js
@@ -2,6 +2,16 @@ const login_btn = document.querySelector("#login-btn");
 const container = document.querySelector(".container");
 
 login_btn.addEventListener('click', () =>{
-    
+    httpGetAsync("http://localhost/agenda/api.php")
 });
 
+function httpGetAsync(theUrl, callback)
+{
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function() { 
+        if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+            callback(xmlHttp.responseText);
+    }
+    xmlHttp.open("GET", theUrl, true); // true for asynchronous 
+    xmlHttp.send(null);
+}
